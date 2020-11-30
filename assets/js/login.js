67d8b90c38aae778c60b3d86c8b07b54ebedcd2d
lvfei <lvfei@qq.com>
2020-11-30 09:37:10 +0800

    完成了登录注册

---
+++ assets/js/login.js
@@ -0,0 +1,57 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    var form = layui.form
+    var layer = layui.layer
+    // 通过layui自定义函数校验规则
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        // 2. 发起Ajax的POST请求
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+    $('#form_login').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = 'index.html'
+            }
+        })
+    })
+})
